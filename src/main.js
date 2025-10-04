c70c142769f1b453c4746e8b2ffb41927441e219
Thomas Le Goff <thomaslegoff56@laposte.net>
2025-10-04 17:45:49 +0200

    chao bye

--- src/main.js
+++ src/main.js
@@ -1,7 +1,81 @@
-import StartGame from './game/main';
+import * as Matter from 'matter-js';
 
-document.addEventListener('DOMContentLoaded', () => {
+let Engine = Matter.Engine,
+  Render = Matter.Render,
+  Runner = Matter.Runner,
+  Body = Matter.Body,
+  Composite = Matter.Composite,
+  Composites = Matter.Composites,
+  Constraint = Matter.Constraint,
+  MouseConstraint = Matter.MouseConstraint,
+  Mouse = Matter.Mouse,
+  Bodies = Matter.Bodies;
+// create engine
+let engine = Engine.create(),
+  world = engine.world;
 
-    StartGame('game-container');
+// create renderer
+let render = Render.create({
+  element: document.body,
+  engine: engine,
+  options: {
+    width: 800,
+    height: 600,
+    showAngleIndicator: true,
+    showCollisions: true,
+    showVelocity: true
+  }
+});
 
+Render.run(render);
+
+// create runner
+let runner = Runner.create();
+Runner.run(runner, engine);
+
+// add bodies
+let group = Body.nextGroup(true);
+
+let ropeA = Composites.stack(100, 50, 8, 1, 10, 10, function (x, y) {
+  return Bodies.rectangle(x, y, 50, 20, {
+    collisionFilter: { group: group }, render: {
+      sprite: { texture: "http://localhost:5173/fish.png" }
+    }
+  });
+});
+
+Composites.chain(ropeA, 0.5, 0, -0.5, 0, { stiffness: 0.8, length: 2, render: { type: 'line' } });
+Composite.add(ropeA, Constraint.create({
+  bodyB: ropeA.bodies[0],
+  pointB: { x: -25, y: 0 },
+  pointA: { x: ropeA.bodies[0].position.x, y: ropeA.bodies[0].position.y },
+  stiffness: 0.5
+}));
+
+Composite.add(world, [
+  ropeA,
+  Bodies.rectangle(400, 600, 1200, 50.5, { isStatic: true })
+]);
+
+// add mouse control
+let mouse = Mouse.create(render.canvas),
+  mouseConstraint = MouseConstraint.create(engine, {
+    mouse: mouse,
+    constraint: {
+      stiffness: 0.2,
+      render: {
+        visible: false
+      }
+    }
+  });
+
+Composite.add(world, mouseConstraint);
+
+// keep the mouse in sync with rendering
+render.mouse = mouse;
+
+// fit the render viewport to the scene
+Render.lookAt(render, {
+  min: { x: 0, y: 0 },
+  max: { x: 700, y: 600 }
 });
