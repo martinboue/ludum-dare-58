5b32d1a6be6e40c9ca28261045b77ca309b90ead
Martin Boué <martbou35@gmail.com>
2025-10-05 10:52:13 +0200

    feat: init level

---
+++ resources/tiles.tsx
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="tiles" tilewidth="16" tileheight="16" spacing="1" tilecount="400" columns="20">
+ <image source="kenney_1-bit-platformer-pack/Tilemap/monochrome_tilemap_transparent.png" width="339" height="339"/>
+ <wangsets>
+  <wangset name="Wall" type="mixed" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="195" wangid="0,0,0,0,0,0,0,1"/>
+   <wangtile tileid="196" wangid="1,0,0,0,0,0,0,0"/>
+   <wangtile tileid="197" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="215" wangid="0,0,0,0,0,0,1,0"/>
+   <wangtile tileid="217" wangid="0,0,1,0,0,0,0,0"/>
+   <wangtile tileid="235" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="236" wangid="0,0,0,0,1,0,0,0"/>
+   <wangtile tileid="237" wangid="0,0,0,1,0,0,0,0"/>
+  </wangset>
+ </wangsets>
+</tileset>
--- src/main.js
+++ src/main.js
@@ -11,21 +11,18 @@ let Engine = Matter.Engine,
   Mouse = Matter.Mouse,
   Bodies = Matter.Bodies;
 // create engine
-let engine = Engine.create(),
-  world = engine.world;
+let engine = Engine.create();
+const world = engine.world;
 
 // create renderer
 let render = Render.create({
   element: document.body,
   engine: engine,
   options: {
-    width: 800,
-    height: 600,
-    showAngleIndicator: true,
-    showCollisions: true,
-    showVelocity: true,
+    width: 960,
+    height: 440,
     // Debug :
-    wireframes: false
+    wireframes: false,
   }
 });
 
@@ -47,17 +44,27 @@ let ropeA = Composites.stack(100, 50, 1, 12, 10, 10, function (x, y) {
   });
 });
 
-Composites.chain(ropeA, 0, 0.5, 0, -0.5, { stiffness: 0.8, length: 2, render: { type: 'line' } });
+Composites.chain(ropeA, 0, 0.5, 0, -0.5, { stiffness: 0.8, length: 2, render: { visible: false } });
 Composite.add(ropeA, Constraint.create({
   bodyB: ropeA.bodies[0],
   pointB: { x: 0, y: -8 },
   pointA: { x: ropeA.bodies[0].position.x, y: ropeA.bodies[0].position.y },
-  stiffness: 1
+  stiffness: 1,
+  render: {
+    visible: false
+  }
 }));
 
 Composite.add(world, [
   ropeA,
-  Bodies.rectangle(400, 600, 1200, 50.5, { isStatic: true })
+  // Top wall
+  Bodies.rectangle(render.options.width/2, 0, render.options.width, 50, { isStatic: true }),
+  // Bottom wall
+  Bodies.rectangle(render.options.width/2, render.options.height, render.options.width, 50, { isStatic: true }),
+  // Right wall
+  Bodies.rectangle(render.options.width, render.options.height/2, 50, render.options.height, { isStatic: true }),
+  // Left wall
+  Bodies.rectangle(0, render.options.height/2, 50, render.options.height, { isStatic: true })
 ]);
 
 // add mouse control
@@ -80,5 +87,5 @@ render.mouse = mouse;
 // fit the render viewport to the scene
 Render.lookAt(render, {
   min: { x: 0, y: 0 },
-  max: { x: 700, y: 600 }
+  max: { x: render.options.width, y: render.options.height }
 });
